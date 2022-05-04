d46da7031140382329a95ffae3e5f65f2c4fb626
Raddi Samardjieva Hansen <raddji@Raddis-MacBook-Air.local>
2022-05-04 15:32:36 -0400

    re-fix logic in display

--- client/src/components/CoffeeShopTile.js
+++ client/src/components/CoffeeShopTile.js
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom"
 
 const CoffeeShopTile = (props) => {
   const { name, city, hours, wifi, parking } = props.coffeeShop
-  const wifiDisplay = ""
-  const parkingDisplay = ""
-  wifi ? wifiDisplay = <li>Wifi is available.</li> : wifiDisplay = <li>Wifi not available.</li>
-  parking ? parkingDisplay = <li>Parking is available.</li> : parkingDisplay = <li>Parking not available.</li>
+  const wifiDisplay = wifi ? wifiDisplay = <li>Wifi is available.</li> : wifiDisplay = <li>Wifi not available.</li>
+  const parkingDisplay = parking ? parkingDisplay = <li>Parking is available.</li> : parkingDisplay = <li>Parking not available.</li>
   
   return (
     <div>

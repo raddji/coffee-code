53b03e2a3d233d8b732c327467c02c67f03ed4ba
Raddi Samardjieva Hansen <raddji@Raddis-MacBook-Air.local>
2022-05-04 16:43:57 -0400

    remove empty spaces

--- client/src/components/CoffeeShopShowPage.js
+++ client/src/components/CoffeeShopShowPage.js
@@ -7,14 +7,11 @@ const CoffeeShopShowPage = (props) => {
   const getCoffeeShop = async () => {
     try {
       const response = await fetch(`/api/v1/coffee-shops/${id}`)
-
       if (!response.ok) {
         throw new Error(`${response.status} (${response.statusText})`)
       }
-
       const body = await response.json()
       setCoffeeShop(body.coffeeShop)
-
     } catch(error) {
       console.error(error)
     }
@@ -24,18 +21,18 @@ const CoffeeShopShowPage = (props) => {
     getCoffeeShop()
   }, [])
 
+  const wifiDisplay = coffeeShop.wifi ? "Wifi Available" : "No Wifi"
+  const parkingDisplay = coffeeShop.parking ? "Parking Available" : "No Parking"
+
   return (
     <div>
       <h1>{coffeeShop.name}</h1>
-
       <p>{coffeeShop.address}</p>
       <p>{coffeeShop.city} {coffeeShop.zip}</p>
-
       <p>{coffeeShop.hours}</p>
-
       <ul>
-        {coffeeShop.parking ? <li>Parking Available</li> : <li>No Parking</li>}
-        {coffeeShop.wifi ? <li> Wifi Available</li> : <li>No Wifi</li>}
+        <li>{wifiDisplay}</li>
+        <li>{parkingDisplay}</li>
       </ul>
     </div>
   )

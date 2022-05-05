fbd8f575287004488b0ff372d962187aad624812
timTonelli <102175985+timTonelli@users.noreply.github.com>
2022-05-05 12:39:14 -0400

    minor indentation fixes

--- client/src/components/NewCoffeeShopForm.js
+++ client/src/components/NewCoffeeShopForm.js
@@ -40,87 +40,87 @@ const NewCoffeeShopForm = ({ postCoffeeShop }) => {
   return (
     <form onSubmit={handleSubmit} >
       <label htmlFor="name">
-      Name:
-      <input
-        type="text"
-        id="name"
-        name="name"
-        onChange={handleInputChange}
-        value={newCoffeeShop.name}
-      />
-    </label>
+        Name:
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleInputChange}
+          value={newCoffeeShop.name}
+        />
+      </label>
 
-    <label htmlFor="city">
-      City:
-      <input
-        type="text"
-        id="city"
-        name="city"
-        onChange={handleInputChange}
-        value={newCoffeeShop.city}
-      />
-    </label>
+      <label htmlFor="city">
+        City:
+        <input
+          type="text"
+          id="city"
+          name="city"
+          onChange={handleInputChange}
+          value={newCoffeeShop.city}
+        />
+      </label>
 
-    <label htmlFor="address">
-      Address:
-      <input
-        type="text"
-        id="address"
-        name="address"
-        onChange={handleInputChange}
-        value={newCoffeeShop.address}
-      />
-    </label>
+      <label htmlFor="address">
+        Address:
+        <input
+          type="text"
+          id="address"
+          name="address"
+          onChange={handleInputChange}
+          value={newCoffeeShop.address}
+        />
+      </label>
 
-    <label htmlFor="zip">
-      Zip:
-      <input
-        type="text"
-        id="zip"
-        name="zip"
-        onChange={handleInputChange}
-        value={newCoffeeShop.zip}
-      />
-    </label>
+      <label htmlFor="zip">
+        Zip:
+        <input
+          type="text"
+          id="zip"
+          name="zip"
+          onChange={handleInputChange}
+          value={newCoffeeShop.zip}
+        />
+      </label>
 
-    <label htmlFor="hours">
-      Hours:
-      <input
-        type="text"
-        id="hours"
-        name="hours"
-        onChange={handleInputChange}
-        value={newCoffeeShop.hours}
-      />
-    </label>
+      <label htmlFor="hours">
+        Hours:
+        <input
+          type="text"
+          id="hours"
+          name="hours"
+          onChange={handleInputChange}
+          value={newCoffeeShop.hours}
+        />
+      </label>
 
-    <label htmlFor="wifi">
-      Wifi:
-      <input
-        type="checkbox"
-        id="wifi"
-        name="wifi"
-        onChange={handleInputChange}
-        value={newCoffeeShop.wifi}
-      />
-    </label>
+      <label htmlFor="wifi">
+        Wifi:
+        <input
+          type="checkbox"
+          id="wifi"
+          name="wifi"
+          onChange={handleInputChange}
+          value={newCoffeeShop.wifi}
+        />
+      </label>
 
-    <label htmlFor="parking">
-      Parking:
-      <input
-        type="checkbox"
-        id="parking"
-        name="parking"
-        onChange={handleInputChange}
-        value={newCoffeeShop.parking}
-      />
-    </label>
+      <label htmlFor="parking">
+        Parking:
+        <input
+          type="checkbox"
+          id="parking"
+          name="parking"
+          onChange={handleInputChange}
+          value={newCoffeeShop.parking}
+        />
+      </label>
 
-    <input
-      className="button"
-      type="submit"
-      value="Submit"
-    />
+      <input
+        className="button"
+        type="submit"
+        value="Submit"
+      />
 
     </form>
   )

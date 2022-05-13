8e98f129844aca9336c261c359e6d83708fa8908
timTonelli <102175985+timTonelli@users.noreply.github.com>
2022-05-13 08:44:48 -0400

    remove random console logs from debugging

--- server/src/routes/api/v1/reviewVotesRouter.js
+++ server/src/routes/api/v1/reviewVotesRouter.js
@@ -7,8 +7,7 @@ const reviewVotesRouter = new express.Router({ mergeParams: true })
 reviewVotesRouter.post("/", async (req, res) => {
   const { reviewId } = req.params
   const userId = req.user.id
-  console.log("REVIEW ID", reviewId)
-  console.log("USER ID", userId)
+  
   try {
     if (req.body.userVoteRecordExists) {
       await Vote.query().patch({ value: req.body.voteValue}).findOne({ reviewId, userId })
@@ -17,7 +16,6 @@ reviewVotesRouter.post("/", async (req, res) => {
     }
     return res.status(201).json({ message: "successful vote"})
   } catch(errors) {
-    console.log(errors)
     if (errors instanceof ValidationError) {
       return res.status(422).json({ errors: errors.data })
     }
